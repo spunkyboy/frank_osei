ddf005d200afaf30728f9f944cd78d5a12e26270
frank Osei <frankosei@franks-MBP.powerhub>
2026-05-18 09:29:09 +0100

    changes to form contact

---
+++ backend/server.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors({
+    origin: "*"
+}));
+
+app.use(express.json());
+
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+const ContactSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    message: String,
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Contact = mongoose.model("Contact", ContactSchema);
+
+app.post("/contact", async (req, res) => {
+    try {
+        const { name, email, message } = req.body;
+
+        const newMessage = new Contact({
+            name,
+            email,
+            message
+        });
+
+        await newMessage.save();
+
+        res.status(201).json({
+            success: true,
+            message: "Message saved successfully"
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Server Error"
+        });
+    }
+});
+
+const PORT = 5003;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});

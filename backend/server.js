85c370b363c61e97b4c6245b40b18c0635299055
frank Osei <frankosei@franks-MBP.powerhub>
2026-05-28 13:43:24 +0100

    changes to backend

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,9 @@ app.use(cors({
 
 app.use(express.json());
 
+app.get("/", (req, res) => {
+    res.send("API is running 🚀");
+});
 
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB Connected"))
